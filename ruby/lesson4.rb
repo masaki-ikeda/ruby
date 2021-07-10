05b403260830468661b5cbf476874a6fd297326c
masaki-ikeda <86770706+masaki-ikeda@users.noreply.github.com>
2021-07-10 22:03:06 +0900

    Add files via upload

---
+++ ruby/lesson4.rb
@@ -0,0 +1,2 @@
+name = "MASAKI IKEDA"
+puts name
---
+++ ruby/lesson6.rb
@@ -0,0 +1,9 @@
+total_plaice = 100
+if total_plaice > 100
+  puts "みかんを購入。所持金にあまりありせん。"
+elsif total_plaice == 100
+  puts "みかんを購入。所持金は０円"
+else
+  puts "みかんを購入することができません"
+
+end
